d507ddf72cba2ce923523a69f302326616e36996
C0rt4ct1n <82264315+c2x2n@users.noreply.github.com>
2025-04-03 20:44:03 +0800

    fix: player is too slow

--- common/src/constants.ts
+++ common/src/constants.ts
@@ -6,12 +6,19 @@ export enum EntityType {
 export const GameConstants = {
     maxPosition: 2048,
     player: {
-        radius: 25,
+        radius: 2,
         defaultHealth: 100,
         maxHealth: 100,
-        speed: 0.5,
+        speed: 10,
         defaultName: "Player",
         maxNameLength: 20
+    },
+    petal: {
+        rotationRadius: 0.8
+    },
+    game: {
+        width: 100,
+        height: 100
     },
     maxTokenLength: 20
 };
