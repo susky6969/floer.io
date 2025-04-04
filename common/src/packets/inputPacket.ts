74b833517d770fc2e01aef40f7cc043b5a59fe46
C0rt4ct1n <82264315+c2x2n@users.noreply.github.com>
2025-04-04 13:55:59 +0800

    feat: inputPacket.mouseDistance

--- common/src/packets/inputPacket.ts
+++ common/src/packets/inputPacket.ts
@@ -3,6 +3,7 @@ import { Vec2 } from "../utils/vector";
 
 export class InputPacket implements Packet {
     direction = Vec2.new(0, 0);
+    mouseDistance: number = 0;
     isAttacking = false;
     isDefending = false;
 
@@ -10,11 +11,13 @@ export class InputPacket implements Packet {
         stream.writeBoolean(this.isAttacking);
         stream.writeBoolean(this.isDefending);
         stream.writeUnit(this.direction, 16);
+        stream.writeUint8(this.mouseDistance);
     }
 
     deserialize(stream: GameBitStream): void {
         this.isAttacking = stream.readBoolean();
         this.isDefending = stream.readBoolean();
         this.direction = stream.readUnit(16);
+        this.mouseDistance = stream.readUint8();
     }
 }
