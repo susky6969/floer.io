95408b5dcf112923d3cb1bd5b9dc18b79eea5eb5
C0rt4ct1n <82264315+c2x2n@users.noreply.github.com>
2025-04-04 13:51:01 +0800

    renamed render/petal.ts

--- client/src/scripts/render/petalRender.ts
+++ client/src/scripts/render/petalRender.ts
@@ -1,17 +1,24 @@
 import { Game } from "@/scripts/game";
 import $ from "jquery";
 import { Vec2 } from "@common/utils/vector";
-import { P2, Graphics } from "@common/utils/math";
+import { P2, MathGraphics } from "@common/utils/math";
+import { GameConstants } from "@common/constants.ts";
 
 const light_pieces = [0, 1, 2, 3, 4];
 
 export function updateEquipPetalColumn(game: Game) {
-    const column = game.ui.equip_petal_column;
-    column.children().remove();
+    const column = game.ui.equipPetalColumn;
+    // column.children().remove();
     let index = 0;
     [0, 1, 2, 3, 4].forEach(() => {
-        const petal_box = $(`<div class="equip-petals equip-${index}" rarity="none" petalName="Light"></div>`);
-        const center = Vec2.new(20, 15);
+        const petal_slot = $(
+            `<div class="equipped-petal-slot"></div>`
+        );
+        const petal_box = $(
+            `<div class="petal rarity-common" petalName="Light"></div>`
+        );
+
+        const center = Vec2.new(19, 14);
         const radius = 8;
 
         const height = 50;
@@ -20,14 +27,15 @@ export function updateEquipPetalColumn(game: Game) {
         const count = light_pieces.length;
 
         let rad = 0;
+        petal_slot.append(petal_box);
 
-        column.append(petal_box);
+        column.append(petal_slot);
 
         light_pieces.forEach(() => {
             const petal = $("<img alt='' class='piece-petal' src='../../../public/img/game/petal/light.svg'>");
-            petal.css("width", "20%");
-            petal.css("height", "20%");
-            const { x, y } = Vec2.add(center, Graphics.getPositionOnCircle(rad, radius));
+            petal.css("width", "25%");
+            petal.css("height", "25%");
+            const { x, y } = Vec2.add(center, MathGraphics.getPositionOnCircle(rad, radius));
 
             petal.css("top", `${y / height * 100}%`);
             petal.css("left", `${x / width * 100}%`);
