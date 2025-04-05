6dda78465da1a2cba8cbb6d58e5d66d1f26723ac
C0rt4ct1n <82264315+c2x2n@users.noreply.github.com>
2025-04-05 16:24:41 +0800

    refactor: keep DRY (4)

--- client/src/scripts/render/petalRender.ts
+++ client/src/scripts/render/petalRender.ts
@@ -4,65 +4,54 @@ import { Vec2 } from "@common/utils/vector";
 import { P2, MathGraphics } from "@common/utils/math";
 import { PetalDefinition, Petals } from "@common/definitions/petal.ts";
 
-const light_pieces = [0, 1, 2, 3, 4];
+const defaultCenter = Vec2.new(25, 21);
+
+const defaultRadius = 8;
+const defaultBoxSize = 50;
+
+export function renderPetalPiece(
+    xOffset: number, yOffset: number, displaySize: number, petal: PetalDefinition, petal_box: JQuery
+){
+    const sizePercent = displaySize;
+    const size = sizePercent / 100 * defaultBoxSize / 2;
+    const center = Vec2.sub(defaultCenter, Vec2.new(size, size));
+
+    const piece = $(`<img alt='' class='piece-petal' src='/img/game/petal/${petal.idString}.svg'>`);
+    piece.css("width", `${ sizePercent }%`);
+    piece.css("height", `${ sizePercent }%`);
+    const { x, y } = center;
+
+    piece.css("top", `${ (y + yOffset) / defaultBoxSize * 100 }%`);
+    piece.css("left", `${ (x + xOffset) / defaultBoxSize * 100 }%`);
+
+    petal_box.append(piece);
+}
 
 export function renderPetal(petal: PetalDefinition) {
     const petal_box = $<HTMLDivElement>(
         `<div class="petal rarity-common" petalName="${petal.displayName}"></div>`
     );
 
-    const defaultCenter = Vec2.new(25, 21);
-
-    const defaultRadius = 8;
-    const defaultBoxSize = 50;
-
     if (petal.isDuplicate) {
-
-
         let radiansNow = 0;
         const count = petal.pieceAmount;
 
-        const sizePercent = 25;
-        const size = sizePercent / 100 * defaultBoxSize / 2;
-        const center = Vec2.sub(defaultCenter, Vec2.new(size, size));
-
         for (let i = 0; i < count; i++) {
-            const piece = $(`<img alt='' class='piece-petal' src='/img/game/petal/${petal.idString}.svg'>`);
-            piece.css("width", `${ sizePercent }%`);
-            piece.css("height", `${ sizePercent }%`);
-            const { x, y } =
-                Vec2.add(center, MathGraphics.getPositionOnCircle(radiansNow, defaultRadius));
-
-            piece.css("top", `${ y / defaultBoxSize * 100 }%`);
-            piece.css("left", `${ x / defaultBoxSize * 100 }%`);
+            const { x, y } = MathGraphics.getPositionOnCircle(radiansNow, defaultRadius)
+            renderPetalPiece(x, y,25, petal, petal_box);
 
             radiansNow += P2 / count;
-
-            petal_box.append(piece);
         }
     } else {
-
-        const sizePercent = petal.displaySize;
-        const size = sizePercent / 100 * defaultBoxSize / 2;
-        const center = Vec2.sub(defaultCenter, Vec2.new(size, size));
-
-        const piece = $(`<img alt='' class='piece-petal' src='/img/game/petal/${petal.idString}.svg'>`);
-        piece.css("width", `${ sizePercent }%`);
-        piece.css("height", `${ sizePercent }%`);
-        const { x, y } = center;
-
-        piece.css("top", `${ y / defaultBoxSize * 100 }%`);
-        piece.css("left", `${ x / defaultBoxSize * 100 }%`);
-
-        petal_box.append(piece);
+        renderPetalPiece(0, 0, petal.displaySize, petal, petal_box);
     }
 
     return petal_box;
 }
 
-export function updateEquipPetalColumn(game: Game) {
-    const equipColumn = game.ui.equipPetalRow;
-    equipColumn.children().remove();
+export function updatePetalRows(game: Game) {
+    const equipRow = game.ui.equipPetalRow;
+    equipRow.children().remove();
     const equpping_petals = ["light", "stinger", "sand", "sand", "rose"].map(
         (e) => Petals.fromString(e)
     );
@@ -73,7 +62,7 @@ export function updateEquipPetalColumn(game: Game) {
 
         petal_slot.append(renderPetal(petal));
 
-        equipColumn.append(petal_slot);
+        equipRow.append(petal_slot);
     })
 
     const prepareRow = game.ui.preparePetalRow;
@@ -85,8 +74,6 @@ export function updateEquipPetalColumn(game: Game) {
         const petal_slot = $(
             `<div class="equipped-petal-slot"></div>`
         );
-
-
 
         prepareRow.append(petal_slot);
     })
