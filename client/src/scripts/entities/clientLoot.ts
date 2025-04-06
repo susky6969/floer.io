c2518478a9166eb784210228967e5510d923f0ba
C0rt4ct1n <82264315+c2x2n@users.noreply.github.com>
2025-04-06 18:01:23 +0800

    LOOT entity was done

--- client/src/scripts/entities/clientLoot.ts
+++ client/src/scripts/entities/clientLoot.ts
@@ -1,37 +1,109 @@
 import { ClientEntity } from "./clientEntity";
 import { EntityType } from "@common/constants";
-import { GameSprite } from "@/scripts/utils/pixi";
+import { GameSprite, getGameAssetsPath } from "@/scripts/utils/pixi";
 import { Game } from "@/scripts/game";
 import { EntitiesNetData } from "@common/packets/updatePacket.ts";
 import { Camera } from "@/scripts/render/camera.ts";
 import { PetalDefinition } from "@common/definitions/petal.ts";
-import { Graphics } from "pixi.js";
+import { Graphics, Text, Container } from "pixi.js";
+import { Vec2 } from "@common/utils/vector.ts";
+import { MathGraphics, P2 } from "@common/utils/math.ts";
+
+const defaultCenter = Vec2.new(0, -4);
+
+const defaultRadius = 8;
+const defaultBoxSize = 50;
+
+function drawPetalPiece(
+    xOffset: number, yOffset: number, displaySize: number, petal: PetalDefinition
+) {
+    const size = GameSprite.getScaleByUnitRadius(displaySize / defaultBoxSize / 2);
+    const center = Vec2.sub(defaultCenter, size);
+
+    const piece = new GameSprite(getGameAssetsPath("petal", petal));
+
+    piece.scale = size;
+    const { x, y } = center;
+    piece.position.set(x + xOffset, y + yOffset);
+
+    return piece;
+}
+
+function drawPetal(petal_box: Container, petal: PetalDefinition) {
+    if (petal.isDuplicate) {
+        let radiansNow = 0;
+        const count = petal.pieceAmount;
+
+        for (let i = 0; i < count; i++) {
+            const { x, y } = MathGraphics.getPositionOnCircle(radiansNow, defaultRadius)
+            petal_box.addChild(
+                drawPetalPiece(x, y,25, petal)
+            );
+
+            radiansNow += P2 / count;
+        }
+    } else {
+        petal_box.addChild(
+            drawPetalPiece(0, 0,petal.displaySize, petal)
+        );
+    }
+
+    return petal_box;
+}
 
 export class ClientLoot extends ClientEntity {
     type = EntityType.Loot;
 
-    images = {
-        body: new GameSprite(),
-        background: new Graphics()
-    };
+    background = new Graphics();
+
+    pieces: Container = new Container();
+
+    name: Text = new Text({
+        style: {
+            fontFamily: 'Ubuntu',
+            fontSize: 10,
+            fill: "#fff",
+            stroke: {color: "#000", width: 2}
+        }
+    });
 
     definition!: PetalDefinition;
 
     constructor(game: Game, id: number) {
         super(game, id);
-        this.images.body.setZIndex(0);
-        this.images.background.zIndex = -1;
 
         this.game.camera.addObject(this.container);
-
-        this.container.addChild(
-            this.images.body,
-            this.images.background
-        );
     }
 
     render(): void {
-        this.container.position =Camera.vecToScreen(this.position);
+        this.container.position = Camera.vecToScreen(this.position);
+    }
+
+    init(): void{
+        this.background.clear()
+            .rect(-25, -25, 50, 50)
+            .fill(0x66c258)
+            .rect(-22, -22, 44, 44)
+            .fill(0x7eef6d);
+
+        this.container.zIndex = -1;
+
+        this.background.zIndex = -1;
+
+        drawPetal(this.container, this.definition);
+
+        this.name.text = this.definition.displayName;
+
+        this.name.anchor.set(0.5);
+        this.name.position.set(0, 13)
+
+        this.name.zIndex = 0;
+
+        this.container.addChild(
+            this.pieces,
+            this.background,
+            this.name
+        );
     }
 
     updateFromData(data: EntitiesNetData[EntityType.Petal], isNew: boolean): void {
@@ -40,13 +112,7 @@ export class ClientLoot extends ClientEntity {
         if (isNew){
             this.definition = data.definition;
 
-            this.images.body
-                .setFrame(`${data.definition.idString}.svg`)
-                .setScaleByUnitRadius(data.definition.hitboxRadius)
-
-            this.images.background.clear()
-                .rect(-25, -25, 50, 50)
-                .fill(0xff0000);
+            this.init();
         }
     }
 }
