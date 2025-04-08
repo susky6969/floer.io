ec1084a7b94c264609fd9f791204d40594654c98
C0rt4ct1n <82264315+c2x2n@users.noreply.github.com>
2025-04-08 20:53:39 +0800

    feat: added rarities

---
+++ common/src/definitions/rarity.ts
@@ -0,0 +1,49 @@
+import { Definitions, ObjectDefinition } from "../utils/definitions";
+
+export enum Rarities {
+    common = "common",
+    unusual = "unusual",
+    rare = "rare",
+    epic = "epic",
+    super = "super"
+}
+
+type RarityDefinition = ObjectDefinition & {
+    idString: Rarities
+} & {
+    color: string;
+    border: string;
+};
+
+export const RarityDefinitions = new Definitions<RarityDefinition>([
+    {
+        idString: Rarities.common,
+        displayName: "Common",
+        color: "#7eef6d",
+        border: "#66c258",
+    },
+    {
+        idString: Rarities.unusual,
+        displayName: "Unusual",
+        color: "#ffe65d",
+        border: "#cfba4b",
+    },
+    {
+        idString: Rarities.rare,
+        displayName: "Rare",
+        color: "#4d52e3",
+        border: "#3e42b8",
+    },
+    {
+        idString: Rarities.epic,
+        displayName: "Epic",
+        color: "#861fde",
+        border: "#6d19b4",
+    },
+    {
+        idString: Rarities.super,
+        displayName: "Super",
+        color: "#2bffa3",
+        border: "#23cf84",
+    }
+] as RarityDefinition[]);
