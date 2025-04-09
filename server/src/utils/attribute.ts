d338d23bd96489d3418248afb02568f25edb08bd
C0rt4ct1n <82264315+c2x2n@users.noreply.github.com>
2025-04-09 22:33:21 +0800

    feat: new events and new realizes

--- server/src/utils/attribute.ts
+++ server/src/utils/attribute.ts
@@ -1,13 +1,20 @@
 import { ServerPetal } from "../entities/serverPetal";
 import { MathGraphics } from "../../../common/src/utils/math";
 import { Vec2 } from "../../../common/src/utils/vector";
-import { PetalAttributeName } from "../../../common/src/definitions/attribute";
-import { PetalAttributeData } from "../../../common/src/definitions/petal";
+import { AttributeName } from "../../../common/src/definitions/attribute";
+import { AttributeData } from "../../../common/src/definitions/petal";
 import { EventInitializer } from "./eventManager";
+import { Effect } from "./effects";
+import { EntityType } from "../../../common/src/constants";
+import { ServerPlayer } from "../entities/serverPlayer";
+import { ServerMob } from "../entities/serverMob";
 
-export enum PetalAttributeEvents {
+export enum AttributeEvents {
     HEALING = "HEALING",
-    DEFEND = "DEFEND"
+    DEFEND = "DEFEND",
+    PETAL_DEAL_DAMAGE = "PETAL_DEAL_DAMAGE",
+    FLOWER_DEAL_DAMAGE = "FLOWER_DEAL_DAMAGE",
+    FLOWER_GET_DAMAGE = "FLOWER_GET_DAMAGE"
 }
 
 export enum PetalUsingAnimations {
@@ -15,33 +22,128 @@ export enum PetalUsingAnimations {
     NORMAL = "NORMAL"
 }
 
-export interface PetalAttributeRealize {
+export interface AttributeRealize<T extends AttributeName = AttributeName> {
+    readonly unstackable?: boolean;
     readonly callback: (
-        on: EventInitializer, petal: ServerPetal, data: PetalAttributeData[PetalAttributeName]
+        on: EventInitializer, petal: ServerPetal, data: AttributeData[T]
     ) => void
 }
 
-export const PetalAttributeRealizes: {[K in PetalAttributeName]: PetalAttributeRealize} = {
-    "absorbing_heal": {
-        callback: (on, petal, data: PetalAttributeData["absorbing_heal"]) => {
-            on(PetalAttributeEvents.HEALING, () => {
-                if (data) petal.owner.health += data;
-            }, PetalUsingAnimations.ABSORB);
+export const PetalAttributeRealizes: {[K in AttributeName]: AttributeRealize<K>} = {
+    absorbing_heal: {
+        callback: (on, petal, data) => {
+            on(AttributeEvents.HEALING,
+                () => {
+                    if (data) petal.owner.heal(data)
+                }
+            , PetalUsingAnimations.ABSORB);
         }
     },
 
-    "boost": {
-        callback: (on, petal, data: PetalAttributeData["boost"]) => {
-            on(PetalAttributeEvents.DEFEND, () => {
-                if (data) {
-                    const direction =
-                        MathGraphics.directionBetweenPoints(petal.owner.position, petal.position);
-                    petal.owner.position = Vec2.add(
-                        petal.owner.position,
-                        Vec2.mul(direction, data)
-                    )
+    boost: {
+        callback: (on, petal, data) => {
+            on(AttributeEvents.DEFEND,
+                () => {
+                    if (data) {
+                        const direction =
+                            MathGraphics.directionBetweenPoints(petal.owner.position, petal.position);
+                        petal.owner.position = Vec2.add(
+                            petal.owner.position,
+                            Vec2.mul(direction, data)
+                        )
+                    }
                 }
-            }, PetalUsingAnimations.NORMAL);
+            , PetalUsingAnimations.NORMAL);
+        }
+    },
+
+    poison: {
+        callback: (on, petal, data) => {
+            on<AttributeEvents.PETAL_DEAL_DAMAGE>(
+                AttributeEvents.PETAL_DEAL_DAMAGE,
+                (entity) => {
+                    if (entity && data) {
+                        new Effect({
+                            effectedTarget: entity,
+                            func: (dt, effected) => {
+                                if (!data) return;
+                                if (effected instanceof ServerPlayer || effected instanceof ServerMob){
+                                    if (!effected.canReceiveDamageFrom(petal)) return
+                                    effected.receiveDamage(dt * data.damagePerSecond, petal.owner);
+                                }
+                            },
+                            duration: data.duration,
+                            workingType: [EntityType.Player, EntityType.Mob]
+                        }).start();
+                    }
+                }
+            )
+        }
+    },
+
+    healing_debuff: {
+        callback: (on, petal, data) => {
+            on<AttributeEvents.PETAL_DEAL_DAMAGE>(
+                AttributeEvents.PETAL_DEAL_DAMAGE,
+                (entity) => {
+                    if (entity && data) {
+                        new Effect({
+                            effectedTarget: entity,
+                            modifier: {
+                                healing: data.healing,
+                            },
+                            duration: data.duration,
+                            workingType: [EntityType.Player]
+                        }).start();
+                    }
+                }
+            )
+        }
+    },
+
+    body_poison: {
+        callback: (on, petal, data) => {
+            on<AttributeEvents.FLOWER_DEAL_DAMAGE>(
+                AttributeEvents.FLOWER_DEAL_DAMAGE,
+                (entity) => {
+                    if (entity && data) {
+                        new Effect({
+                            effectedTarget: entity,
+                            func: (dt, effected) => {
+                                if (!data) return;
+                                if (
+                                    effected instanceof ServerPlayer
+                                    || effected instanceof ServerMob
+                                ){
+                                    if (!effected.canReceiveDamageFrom(petal)) return
+                                    effected.receiveDamage(dt * data.damagePerSecond, petal.owner);
+                                }
+                            },
+                            duration: data.duration,
+                            workingType: [EntityType.Player, EntityType.Mob]
+                        }).start();
+                    }
+                }
+            )
+        }
+    },
+
+    damage_reflection: {
+        callback: (on, petal, data) => {
+            on<AttributeEvents.FLOWER_GET_DAMAGE>(AttributeEvents.FLOWER_GET_DAMAGE,
+                (arg) => {
+                    if (arg && data) {
+                        const { entity, damage } = arg;
+                        if (
+                            entity instanceof ServerPlayer
+                            || entity instanceof ServerMob
+                            && entity.canReceiveDamageFrom(petal.owner)
+                        ) {
+                            entity.receiveDamage(data * damage, petal.owner)
+                        }
+                    }
+                }
+            )
         }
     }
 } as const;
