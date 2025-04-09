ccc2ed7fbe46d589e892c2ccebaf64bc4fcb9bf3
C0rt4ct1n <82264315+c2x2n@users.noreply.github.com>
2025-04-09 22:34:44 +0800

    feat: 9 new petals (1 dev only) and fix: basic wont drop now

--- common/src/definitions/petal.ts
+++ common/src/definitions/petal.ts
@@ -1,6 +1,7 @@
 import { Definitions, ObjectDefinition } from "../utils/definitions";
-import { PetalAttributeName } from "./attribute";
+import { AttributeName } from "./attribute";
 import { Rarities } from "./rarity";
+import { PlayerModifiers } from "../typings";
 
 export type SavedPetalDefinitionData = PetalDefinition | null
 
@@ -11,8 +12,10 @@ export type PetalDefinition = ObjectDefinition & {
     readonly hitboxRadius: number
     readonly extendable: boolean
     readonly rarity: Rarities
-    readonly attributes?: PetalAttributeData
-} & PetalPieceType & PetalAttributeType;
+    readonly attributes?: AttributeData
+    readonly modifiers?: Partial<PlayerModifiers>
+    readonly undroppable?: boolean
+} & PetalPieceType & PetalUsageType;
 
 type PetalPieceType = {
     readonly isDuplicate: false
@@ -26,18 +29,31 @@ type PetalPieceType = {
     readonly isShowedInOne: boolean
 };
 
-type PetalAttributeType = {
+type PetalUsageType = {
     readonly usable: false
 } | {
     readonly usable: true
     readonly useTime: number
 }
 
-export type PetalAttributeData = {
-    [K in PetalAttributeName] ?: unknown
+export type AttributeData = {
+    [K in AttributeName] ?: unknown
 } & ({
     absorbing_heal?: number
-    boost?: number
+    boost?: number,
+    poison?: {
+        damagePerSecond: number
+        duration: number
+    }
+    healing_debuff?: {
+        healing: number
+        duration: number
+    }
+    body_poison?: {
+        damagePerSecond: number
+        duration: number
+    }
+    damage_reflection?: number
 })
 
 export function getDisplayedPieces(petal: PetalDefinition): number {
@@ -107,6 +123,24 @@ export const Petals = new Definitions<PetalDefinition>([
         isDuplicate: false,
         pieceAmount: 1,
         rarity: Rarities.unusual
+    },{
+        idString: "tri_rose",
+        displayName: "Rose",
+        damage: 5,
+        health: 5,
+        extendable: false,
+        usable: true,
+        useTime: 1,
+        attributes: {
+            absorbing_heal: 10.5
+        },
+        reloadTime: 3.5,
+        hitboxRadius: 0.4,
+        isDuplicate: true,
+        isShowedInOne: true,
+        pieceAmount: 3,
+        rarity: Rarities.rare,
+        usingAssets: "rose"
     },
     {
         idString: "bubble",
@@ -159,6 +193,141 @@ export const Petals = new Definitions<PetalDefinition>([
         displaySize: 45,
         isDuplicate: false,
         pieceAmount: 1,
+        undroppable: true,
         rarity: Rarities.common
     },
+    {
+        idString: "dandelion",
+        displayName: "Dandelion",
+        damage: 5,
+        health: 5,
+        extendable: true,
+        selfRotation: 0.02,
+        usable: false,
+        attributes: {
+            healing_debuff: {
+                healing: 0.8,
+                duration: 10,
+            }
+        },
+        reloadTime: 2,
+        hitboxRadius: 0.5,
+        displaySize: 45,
+        isDuplicate: false,
+        pieceAmount: 1,
+        rarity: Rarities.rare,
+    }, {
+        idString: "iris",
+        displayName: "Iris",
+        damage: 5,
+        health: 5,
+        extendable: true,
+        selfRotation: 0,
+        usable: false,
+        attributes: {
+            poison: {
+                damagePerSecond: 9,
+                duration: 6.5
+            }
+        },
+        reloadTime: 6,
+        hitboxRadius: 0.4,
+        displaySize: 25,
+        isDuplicate: false,
+        pieceAmount: 1,
+        rarity: Rarities.unusual,
+    }, {
+        idString: "cactus",
+        displayName: "Cactus",
+        damage: 5,
+        health: 15,
+        extendable: true,
+        selfRotation: 0,
+        usable: false,
+        modifiers: {
+            maxHealth: 20
+        },
+        reloadTime: 1,
+        hitboxRadius: 0.6,
+        displaySize: 52,
+        isDuplicate: false,
+        pieceAmount: 1,
+        rarity: Rarities.rare,
+    }, {
+        idString: "poison_cactus",
+        displayName: "Cactus",
+        damage: 5,
+        health: 15,
+        extendable: true,
+        selfRotation: 0,
+        usable: false,
+        attributes: {
+            poison: {
+                damagePerSecond: 10,
+                duration: 0.6
+            },
+            body_poison: {
+                damagePerSecond: 9,
+                duration: 4.5
+            }
+        },
+        modifiers: {
+            maxHealth: 20
+        },
+        reloadTime: 1,
+        hitboxRadius: 0.6,
+        displaySize: 52,
+        isDuplicate: false,
+        pieceAmount: 1,
+        rarity: Rarities.epic,
+    }, {
+        idString: "tri_cactus",
+        displayName: "Cactus",
+        damage: 5,
+        health: 15,
+        extendable: true,
+        usable: false,
+        modifiers: {
+            maxHealth: 20
+        },
+        reloadTime: 1,
+        hitboxRadius: 0.6,
+        isDuplicate: true,
+        isShowedInOne: true,
+        pieceAmount: 3,
+        rarity: Rarities.legendary,
+        usingAssets: "cactus"
+    },{
+        idString: "salt",
+        displayName: "Salt",
+        damage: 10,
+        health: 10,
+        extendable: true,
+        selfRotation: 0,
+        usable: false,
+        attributes:{
+            damage_reflection: 0.25
+        },
+        reloadTime: 2.5,
+        displaySize: 40,
+        hitboxRadius: 0.6,
+        isDuplicate: false,
+        pieceAmount: 1,
+        rarity: Rarities.rare
+    },
+    {
+        idString: "tri_stinger",
+        displayName: "Stinger",
+        damage: 35,
+        health: 8,
+        extendable: true,
+        reloadTime: 4,
+        usable: false,
+        hitboxRadius: 0.3,
+        isDuplicate: true,
+        isShowedInOne: true,
+        pieceAmount: 3,
+        rarity: Rarities.legendary,
+        usingAssets: "stinger"
+    }
 ] satisfies PetalDefinition[]);
