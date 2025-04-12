447456af4dcc55a0b0dae7faf06f60d5428743de
C0rt4ct1n <82264315+c2x2n@users.noreply.github.com>
2025-04-12 20:54:02 +0800

    feat: expWhenAbsorb

--- common/src/definitions/rarity.ts
+++ common/src/definitions/rarity.ts
@@ -14,6 +14,7 @@ type RarityDefinition = ObjectDefinition & {
 } & {
     color: string;
     border: string;
+    expWhenAbsorb: number;
 };
 
 export const Rarity = new Definitions<RarityDefinition>([
@@ -22,35 +23,41 @@ export const Rarity = new Definitions<RarityDefinition>([
         displayName: "Common",
         color: "#7eef6d",
         border: "#66c258",
+        expWhenAbsorb: 2
     },
     {
         idString: RarityName.unusual,
         displayName: "Unusual",
         color: "#ffe65d",
         border: "#cfba4b",
+        expWhenAbsorb: 10
     },
     {
         idString: RarityName.rare,
         displayName: "Rare",
         color: "#4d52e3",
         border: "#3e42b8",
+        expWhenAbsorb: 50
     },
     {
         idString: RarityName.epic,
         displayName: "Epic",
         color: "#861fde",
         border: "#6d19b4",
+        expWhenAbsorb: 200
     },
     {
         idString: RarityName.legendary,
         displayName: "Legendary",
         color: "#de1f1f",
         border: "#b41919",
+        expWhenAbsorb: 1000
     },
     {
         idString: RarityName.super,
         displayName: "Super",
         color: "#2bffa3",
         border: "#23cf84",
+        expWhenAbsorb: 10000
     }
 ] as RarityDefinition[]);
