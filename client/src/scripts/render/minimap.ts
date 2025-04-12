d9bdeed089a5e2ef050ad020450f5acb80bc0d97
C0rt4ct1n <82264315+c2x2n@users.noreply.github.com>
2025-04-12 20:57:31 +0800

    feat: added zones display in minimap

--- client/src/scripts/render/minimap.ts
+++ client/src/scripts/render/minimap.ts
@@ -1,7 +1,11 @@
 import { Game } from "@/scripts/game.ts";
-import { Graphics, Container } from "pixi.js";
+import { Graphics, Container, Text } from "pixi.js";
 import { MathNumeric } from "@common/utils/math.ts";
 import { Vec2 } from "@common/utils/vector.ts";
+import { GameConstants, Zones } from "@common/constants.ts";
+
+const widthDiv = 12;
+const heightDiv = 3;
 
 export class Minimap {
     mapBackground = new Graphics({
@@ -13,6 +17,8 @@ export class Minimap {
     })
 
     container = new Container();
+
+    mapNames: Text[] = [];
 
     private minimapPositionX: number = 0;
     private minimapPositionY: number = 0;
@@ -28,9 +34,12 @@ export class Minimap {
            this.mapBackground,
            this.playerPosition
        );
+       this.container.zIndex = 999;
+
        this.game.pixi.stage.addChild(
            this.container
        );
+
     }
 
     render(){
@@ -54,27 +63,63 @@ export class Minimap {
         this.width = this.game.width;
         this.height = this.game.height;
 
+        this.minimapWidth = this.width / widthDiv;
+        this.minimapHeight = this.height / heightDiv;
+
         const screenWidth = this.game.pixi.screen.width;
         const screenHeight = this.game.pixi.screen.height;
-
-        this.minimapWidth = this.width / 8;
-        this.minimapHeight = this.height / 2;
 
         this.minimapPositionX = screenWidth - this.minimapWidth - 8;
         this.minimapPositionY = screenHeight - this.minimapHeight - 8;
 
         this.container.position.set(this.minimapPositionX, this.minimapPositionY);
 
+        this.redraw();
+    }
+
+    redraw(): void {
         this.mapBackground.clear()
-            .rect(
+            .roundRect(
                 0,
                 0,
                 this.minimapWidth,
-                this.minimapHeight
+                this.minimapHeight, 2
             )
             .fill({
                 color: 0x000,
-                alpha: 0.5
+                alpha: 0.9
+            })
+            .stroke({
+                color: 0x000,
+                width: 3,
             });
+
+        for (const x in Zones) {
+            const data = Zones[x];
+            this.mapBackground
+                .rect(data.x / widthDiv, 0, data.width / widthDiv, this.minimapHeight)
+                .fill(data.displayColor)
+        }
+
+        let index = 0;
+        for (const x in Zones) {
+            const data = Zones[x];
+            if (!this.mapNames[index]) {
+                this.mapNames[index] = new Text({
+                    text: x,
+                    style: {
+                        fontFamily: 'Ubuntu',
+                        fontSize: 11,
+                        fill: "#fff",
+                        stroke: {color: "#000", width: 2}
+                    }
+                });
+                this.mapNames[index].anchor.set(0.5);
+                this.container.addChild(this.mapNames[index]);
+                this.mapNames[index].zIndex = 3;
+            }
+            this.mapNames[index].position.set(data.x / widthDiv + data.width / widthDiv / 2, this.minimapHeight / 2);
+            index ++;
+        }
     }
 }
