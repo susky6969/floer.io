d303cb0fd25a8ea1916af71937db6d71e5176f32
C0rt4ct1n <82264315+c2x2n@users.noreply.github.com>
2025-04-12 20:49:50 +0800

    feat: Zones

--- common/src/constants.ts
+++ common/src/constants.ts
@@ -1,4 +1,4 @@
-import { PlayerModifiers } from "./typings";
+import { Modifiers } from "./typings";
 
 export enum EntityType {
     Player,
@@ -19,17 +19,19 @@ export const GameConstants = {
         maxNameLength: 20,
         spawnMaxX: 100,
         spawnMaxY: 50,
-        revolutionSpeed: 2,
-        defaultSlot: 5,
-        defaultEquippedPetals: ["basic","basic","basic","basic","basic"],
-        defaultPreparationPetals: ["","","","","","","",""],
+        defaultSlot: 8,
+        defaultPrepareSlot: 8,
+        defaultEquippedPetals: ["basic","basic","basic","basic","basic","basic","basic","basic"],
+        defaultPreparationPetals: [],
         defaultPetalDistance: 3.8,
         defaultPetalAttackingDistance: 6.5,
         defaultPetalDefendingDistance: 2.5,
-        defaultModifiers: (): PlayerModifiers => ({
+        defaultModifiers: (): Modifiers => ({
             healing: 1,
             maxHealth: 150,
-            healPerSecond: 0.1
+            healPerSecond: 0.1,
+            speed: 1,
+            revolutionSpeed: 2
         })
     },
     petal: {
@@ -37,22 +39,7 @@ export const GameConstants = {
     },
     game: {
         width: 3036,
-        height: 80,
-        spawningMob: {
-            "ladybug": 10,
-            "shiny_ladybug": 0.1,
-            "dark_ladybug": 20,
-            "hornet": 20,
-            "spider": 15,
-            "beetle": 15,
-            "rock": 20,
-            "cactus": 25,
-            "massive_ladybug": 0.1,
-            "bee": 10,
-            "worker_ant": 10,
-            "baby_ant": 20,
-            "soldier_ant": 20,
-        }
+        height: 80
     },
     loot: {
         radius: 0.5,
@@ -62,7 +49,127 @@ export const GameConstants = {
     mob: {
         maxHealth: 100000,
         walkingReload: 2,
-        walkingTime: 1
+        walkingTime: 1,
+        defaultModifiers: (): Modifiers => ({
+            healing: 1,
+            maxHealth: 1,
+            healPerSecond: 0,
+            revolutionSpeed: 0,
+            speed: 1
+        })
     },
     maxTokenLength: 20
 };
+
+export const Zones:
+{
+    [key: string]: {
+        x: number,
+        width: number,
+        displayColor: string,
+        backgroundColor: string,
+        borderColor: string,
+        density: number,
+        spawning: Record<string, number>
+    }
+} = {
+    "Easy": {
+        x: 0,
+        width: 500,
+        displayColor: "#1da25e",
+        backgroundColor: "#29ca77",
+        borderColor: "#1da25e",
+        density: 0.7,
+        spawning: {
+            "ladybug": 10,
+            "rock": 20,
+            "boulder": 1,
+            "massive_ladybug": 0.1,
+            "bee": 10,
+            "worker_ant": 10,
+            "baby_ant": 5,
+            "soldier_ant": 2,
+        }
+    },
+    "Medium": {
+        x: 500,
+        width: 500,
+        displayColor: "#92a728",
+        backgroundColor: "#ecdcb8",
+        borderColor: "#bfb295",
+        density: 0.7,
+        spawning: {
+            "ladybug": 10,
+            "shiny_ladybug": 0.1,
+            "beetle": 5,
+            "cactus": 35,
+            "mega_cactus": 1,
+            "mega_beetle": 0.1,
+            "bee": 10,
+            "worker_ant": 5,
+            "baby_ant": 5,
+            "soldier_ant": 5,
+        }
+    },
+    "Hard": {
+        x: 1000,
+        width: 700,
+        displayColor: "#923a28",
+        backgroundColor: "#9a5951",
+        borderColor: "#742d2d",
+        density: 0.75,
+        spawning: {
+            "ladybug": 10,
+            "dark_ladybug": 20,
+            "massive_dark_ladybug": 1,
+            "hornet": 5,
+            "mega_hornet": 1,
+            "spider": 10,
+            "mega_spider": 1,
+            "beetle": 10,
+            "mega_beetle": 1,
+            "rock": 5,
+            "boulder": 20,
+            "worker_ant": 7,
+            "baby_ant": 7,
+            "soldier_ant": 15,
+        }
+    },
+    "?": {
+        x: 1700,
+        width: 500,
+        displayColor: "#a4aaa6",
+        backgroundColor: "#888484",
+        borderColor: "#484646",
+        density: 0.8,
+        spawning: {
+            "dark_ladybug": 20,
+            "massive_dark_ladybug": 5,
+            "hornet": 20,
+            "mega_hornet": 3,
+            "spider": 15,
+            "mega_spider": 3,
+            "beetle": 15,
+            "mega_beetle": 3,
+            "boulder": 20,
+            "worker_ant": 7,
+            "baby_ant": 7,
+            "soldier_ant": 20,
+        }
+    },
+    "Hornet Hell": {
+        x: 2200,
+        width: 3036 - 2200,
+        displayColor: "#fff04a",
+        backgroundColor: "#ded247",
+        borderColor: "#b6ab44",
+        density: 0.8,
+        spawning: {
+            "hornet": 100,
+            "mega_hornet": 15,
+            "beetle": 15,
+            "mega_beetle": 5,
+            "mega_spider": 5,
+        }
+    }
+}
